295456b12dda0857e9c0ff1378db5fd14838af7b
bahiamartins <bahiamartins@hotmail.com>
2013-08-07 14:09:18 -0300

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
   
 
   describe "when name is not present" do
@@ -97,5 +98,10 @@ describe User do
             specify { expect(user_for_invalid_password).to be_false }
           end
         end
+        
+        describe "remember token" do
+            before { @user.save }
+            its(:remember_token) { should_not be_blank }
+          end
   
 end
